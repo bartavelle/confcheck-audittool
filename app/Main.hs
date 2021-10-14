11420884076843b143f0b30e905ea44d086dfcdc
dev163 <dev163@mail.com>
2021-10-14 11:37:44 +0200

    Ormolu pass

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,7 @@
 module Main where
 
+import AuditTool
 import qualified Data.ByteString.Lazy as BSL
-
-import           AuditTool
 
 main :: IO ()
 main = BSL.readFile "sample/DESKTOP-M00LTUT.xml" >>= mapM_ print . parseAuditTool
